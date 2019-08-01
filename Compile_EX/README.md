60f4ca1924cc2d16c14c90521688406834fe2879
plum <>
2019-08-01 22:13:39 +0800

    探索优化

--- Compile_EX/README.md
+++ Compile_EX/README.md
@@ -1,5 +1,8 @@
-#  <#Title#>
+#  swift  编译优化
 
 
 1. 指明类型 ， 不要让编译器推断
 2. 用if let 取代 ??
+3. 显示编译时间 defaults write com.apple.dt.Xcode ShowBuildOperationDuration -bool YES
+4. -Xfrontend -warn-long-function-bodies=100   ；  -Xfrontend -warn-long-expression-type-checking=100
+## name
--- Compile_EX/ViewController.swift
+++ Compile_EX/ViewController.swift
@@ -12,32 +12,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let myCompany: [String: [String : [String: String]]] = [
-            "employees": [
-                "employee 1": ["attribute": "value"],
-                "employee 2": ["attribute": "value"],
-                "employee 3": ["attribute": "value"],
-                "employee 4": ["attribute": "value"],
-                "employee 5": ["attribute": "value"],
-                "employee 6": ["attribute": "value"],
-                "employee 7": ["attribute": "value"],
-                "employee 8": ["attribute": "value"],
-                "employee 9": ["attribute": "value"],
-                "employee 10": ["attribute": "value"],
-                "employee 11": ["attribute": "value"],
-                "employee 12": ["attribute": "value"],
-                "employee 13": ["attribute": "value"],
-                "employee 14": ["attribute": "value"],
-                "employee 15": ["attribute": "value"],
-                "employee 16": ["attribute": "value"],
-                "employee 17": ["attribute": "value"],
-                "employee 18": ["attribute": "value"],
-                "employee 19": ["attribute": "value"],
-                "employee 20": ["attribute": "value"],
-            ]
-            ]
+//        let myCompany: [String: [String : [String: String]]] = [
+//            "employees": [
+//                "employee 1": ["attribute": "value"],
+//                "employee 2": ["attribute": "value"],
+//                "employee 3": ["attribute": "value"],
+//                "employee 4": ["attribute": "value"],
+//                "employee 5": ["attribute": "value"],
+//                "employee 6": ["attribute": "value"],
+//                "employee 7": ["attribute": "value"],
+//                "employee 8": ["attribute": "value"],
+//                "employee 9": ["attribute": "value"],
+//                "employee 10": ["attribute": "value"],
+//                "employee 11": ["attribute": "value"],
+//                "employee 12": ["attribute": "value"],
+//                "employee 13": ["attribute": "value"],
+//                "employee 14": ["attribute": "value"],
+//                "employee 15": ["attribute": "value"],
+//                "employee 16": ["attribute": "value"],
+//                "employee 17": ["attribute": "value"],
+//                "employee 18": ["attribute": "value"],
+//                "employee 19": ["attribute": "value"],
+//                "employee 20": ["attribute": "value"],
+//            ]
+//            ]
+        
     }
 
 
+//    func count() -> CGFloat {
+//        let hour: CGFloat = 1
+//        let hourDelta: CGFloat = 4
+//        let minute = 20
+//        let minuteDelta = 5
+//        let unit: Int = 5
+//        let mi = CGFloat(M_PI)
+//        let other = CGFloat(hour + hourDelta + CGFloat(minute + minuteDelta) / 60)
+//
+//        return CGFloat(M_PI) * (((hour + hourDelta + (minute + minuteDelta) / 60) * 5) - 15)
+//    }
 }
 
